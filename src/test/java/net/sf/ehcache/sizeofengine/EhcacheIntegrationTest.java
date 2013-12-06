f6f131990c751eb28bdd5c9a01844619b62c10b5
Alex Snaps <alex.snaps@gmail.com>
2013-12-06 11:42:45 -0500

    Added integration test

---
+++ src/test/java/net/sf/ehcache/sizeofengine/EhcacheIntegrationTest.java
@@ -0,0 +1,20 @@
+package net.sf.ehcache.sizeofengine;
+
+import net.sf.ehcache.pool.SizeOfEngine;
+import net.sf.ehcache.pool.SizeOfEngineLoader;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alex Snaps
+ */
+public class EhcacheIntegrationTest {
+
+  @Test
+  public void testEhcacheUsesOurImplementation() {
+    final SizeOfEngine sizeOfEngine = SizeOfEngineLoader.newSizeOfEngine(10, false, false);
+    assertThat(sizeOfEngine, instanceOf(EhcacheSizeOfEngine.class));
+  }
+}
