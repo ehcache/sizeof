2934d95084372e7c209d8f282813e3c3a3c45404
Henri Tremblay <henri.tremblay@gmail.com>
2017-10-04 16:53:12 -0700

    Add the same copyright everywhere

--- src/main/java/org/ehcache/sizeof/FlyWeightType.java
+++ src/main/java/org/ehcache/sizeof/FlyWeightType.java
@@ -1,19 +1,18 @@
 /**
- *  Copyright Terracotta, Inc.
+ * Copyright Terracotta, Inc.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package org.ehcache.sizeof;
 
 import java.lang.reflect.Field;
@@ -60,9 +59,6 @@ enum FlyweightType {
     // XXX There is no nullipotent way of determining the interned status of a string
     // There are numerous String constants within the JDK (see list at http://docs.oracle.com/javase/7/docs/api/constant-values.html),
     // but enumerating all of them would lead to lots of == tests.
-    /**
-     * java.lang.String
-     */
     //STRING(String.class) {
     //    @Override
     //    boolean isShared(final Object obj) { return obj == ((String)obj).intern(); }
